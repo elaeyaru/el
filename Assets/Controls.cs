4bd71ff21c60f5989f8b8be21a7c233862e5be40
el <elaeyaru@gmail.com>
2020-01-12 18:05:54 +0200

    dwf

---
+++ Assets/Controls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+	public float speed;
+	private Rigidbody2D body;
+	private Vector2 Velocity;
+
+	void Start () {
+		body = GetComponent<Rigidbody2D>();
+	}
+
+	void Update () {
+		Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		Velocity = moveInput.normalized * speed;
+	}
+
+	void FixedUpdate () {
+		body.MovePosition (body.position + Velocity * Time.fixedDeltaTime);
+	}
+}
---
+++ Assets/walking.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walking : MonoBehaviour
+{
+  public int food;
+  public float speed;
+
+  public Transform[] moveSpots;
+  private int currentSpot;
+
+
+  // Use this for initialization
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+    if (food == 0)
+    {
+      currentSpot = 0;
+    }
+
+    transform.position = Vector2.MoveTowards(transform.position, moveSpots[currentSpot].position, speed * Time.deltaTime);
+
+    if (Vector2.Distance(transform.position, moveSpots[0].position) < 0.2f && food == 0)
+    {
+      food = 20;
+      currentSpot = 1;
+      Debug.Log(food);
+    }
+    if (Vector2.Distance(transform.position, moveSpots[1].position) < 0.2f && food != 0)
+    {
+      food = 0;
+      currentSpot = 0;
+      Debug.Log(food);
+    }
+
+  }
+}
